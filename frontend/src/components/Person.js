7e67635365af7e1880e83bfe5900fda2b047446a
jkseppal <jani.k.seppala@gmail.com>
2021-11-18 10:21:12 +0200

    -

--- frontend/src/components/Person.js
+++ frontend/src/components/Person.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
+import Togglable from './Togglabble'
 import { useParams } from 'react-router-dom'
+import { Table } from 'react-bootstrap'
 
 const Person = ({ people, user }) => {
   const id = useParams().id
   const person = people.find(p => p.id === id)
+
+  const physRef = useRef()
+  const licRef = useRef()
 
   if (!person) {
     return null
@@ -13,6 +18,52 @@ const Person = ({ people, user }) => {
     <div>
       <h2>{person.surname} {person.firstname}</h2>
       <h3>{person.sosID}</h3>
+      <Togglable buttonLabel='fyysinen toimintakyky' closeText='piilota' ref={physRef}>
+        <Table striped hover>
+          <thead>
+            <tr>
+              <td>päivämäärä</td>
+              <td>pituus (cm)</td>
+              <td>paino (kg)</td>
+              <td>etunojapunnerrukset</td>
+              <td>leuanvedot</td>
+              <td>12 min juoksu (m)</td>
+            </tr>
+          </thead>
+          <tbody>
+            {person.physicals.map(p =>
+              <tr key={p.date}>
+                <td>{p.date}</td>
+                <td>{p.height}</td>
+                <td>{p.weight}</td>
+                <td>{p.pushUps}</td>
+                <td>{p.pullUps}</td>
+                <td>{p.cooper}</td>
+              </tr>
+            )}
+          </tbody>
+        </Table>
+      </Togglable>
+      <Togglable buttonLabel='lisenssit' closeText='piilota' ref={licRef}>
+        <Table striped hover>
+          <thead>
+            <tr>
+              <td>lisenssi</td>
+              <td>myönnetty</td>
+              <td>voimassaolo päättyy</td>
+            </tr>
+          </thead>
+          <tbody>
+            {person.licenses.map(l =>
+              <tr key={l.content}>
+                <td>{l.content}</td>
+                <td>{l.initialDate}</td>
+                <td>{l.expirationDate}</td>
+              </tr>
+            )}
+          </tbody>
+        </Table>
+      </Togglable>
     </div>
   )
 }
---
+++ frontend/src/components/Togglabble.js
@@ -0,0 +1,44 @@
+import React, { useState, useImperativeHandle } from 'react'
+import PropTypes from 'prop-types'
+import { Button } from 'react-bootstrap'
+
+const Togglable = React.forwardRef((props, ref) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  useImperativeHandle(ref, () => {
+    return {
+      toggleVisibility
+    }
+  })
+
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <div className="buttonWrapper">
+          <Button id={props.id} variant="primary" onClick={toggleVisibility}>{props.buttonLabel}</Button>
+        </div>
+      </div>
+      <div style={showWhenVisible}>
+        {props.children}
+        <div className="buttonWrapper">
+          <Button variant="secondary" onClick={toggleVisibility}>{props.closeText}</Button>
+        </div>
+      </div>
+    </div>
+  )
+})
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired
+}
+
+Togglable.displayName = 'Togglable'
+
+export default Togglable
